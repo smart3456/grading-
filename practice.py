d7c57612214b1e03cfda55bdad61cad904557511
smart3456 <isamuhammedmaruf345@gmail.com>
2024-10-05 13:00:40 +0100

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,29 @@
+import time as t
+
+# print(t.time())
+# print(t.localtime())
+# print(t.strftime("%Y-%m-%d %H:%M:%S"))
+print("hello")
+t.sleep(10)
+print("welcome")
+
+
+# from math import *
+# from audioop import bias
+# from math import ceil,floor
+# import random as ran
+# import random as ran
+# print(math.pi)
+# print(math.pow(10,6))
+# print(math.sqrt(5))
+# print(floor(ran.random() * 10) + 1)
+# print(floor(2.5))
+# print(ceil(2.5))
+# print(ran.randint(1,10))
+
+# names = ["shegun","vincent","david","king","queen","saviour"]
+# guess = ran.choice(names)
+# guess = ran.sample(names,2)
+# print(guess)
+# bias = [0,5,1,2,10,9]
+# print(ran.choices(names,bias,k=4))
